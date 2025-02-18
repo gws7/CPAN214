e11fde9aa843ab5e9bee5056f815929aa6452cfc
GabrielSosin-SectionC <n01646959@humber.ca>
2025-02-17 21:06:15 -0500

    Lab4

--- Lab4/divisionCheck.py
+++
@@ -1,9 +0,0 @@
-def divisionCheck(n):
-    if (((n%5)==0) and ((n%3)==0)):
-        return "Divisible by 5 and 3"
-    elif ((n%5)==0):
-        return "Divisible by 5"
-    elif ((n%3)==0):
-        return "Divisible by 3"
-    
-# print(divisionCheck(15))
