1c4d31cf3b6cc9a2c09a3b498c3ad1e40150cd47
GabrielSosin-SectionC <n01646959@humber.ca>
2025-03-22 14:09:34 -0400

    Djangoooooooooooo

--- Lab6/Lab6.py
+++ Lab6/Lab6.py
@@ -3,72 +3,87 @@ basketballTeam5 = {"Gabriel", "Steve", "Mario", "Jack", "Ted"}
 baseballTeam9 = {"Andrew", "Raph", "John", "Jake", "Chad", "Alex", "Kendrick", "Carti", "Diogo"}
 soccerTeam11 = {"Gabriel", "Steve", "John", "Jake", "Chad", "Alex", "Lionel", "Cristiano", "Neymar", "Kylian", "Sergio"}
 
-# ---//---
-
 def fileWriter(file, itemsToAdd):
-    with open(file, "w") as file:
-        for item in itemsToAdd:
-            file.write(item + " ")
-    file.close()
-            
-# fileWriter("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/basketball_set.txt", basketballTeam5)
-# fileWriter("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/baseball_set.txt", baseballTeam9)
-# fileWriter("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/soccer_set.txt", soccerTeam11)
-
+    try:
+        with open(file, "w") as f:
+            for item in itemsToAdd:
+                f.write(f"{item} ")
+    except Exception as e:
+        print(f"Error writing to {file}: {e}")
 
 def fileWriterIntersections(file, set1, set2):
-    intersected = set1 & set2
-    with open(file, "w") as file:
-        for item in intersected:
-            file.write(item + " ")
-    file.close()
-    
-# fileWriterIntersections("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/intersection_basketball_soccer.txt", basketballTeam5, soccerTeam11)
-# fileWriterIntersections("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/intersection_baseball_soccer.txt", baseballTeam9, soccerTeam11)
-
+    try:
+        intersected = set1 & set2
+        with open(file, "w") as f:
+            for item in intersected:
+                f.write(item + " ")
+    except Exception as e:
+        print(f"Error writing to {file}: {e}")
 
 def fileWriterUnion(file, set1, set2):
-    union = set1 | set2
-    with open(file, "w") as file:
-        for item in union:
-            file.write(item + " ")
-    file.close()
-    
-# fileWriterUnion("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/union_basketball_baseball.txt", basketballTeam5, baseballTeam9)
-
+    try:
+        union = set1 | set2
+        with open(file, "w") as f:
+            for item in union:
+                f.write(item + " ")
+    except Exception as e:
+        print(f"Error writing to {file}: {e}")
 
 def fileWriterDifference(file, set1, set2):
-    difference = set1 - set2
-    with open(file, "w") as file:
-        for item in difference:
-            file.write(item + " ")
-    file.close()
-    
-# fileWriterUnion("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/difference_basketball_soccer.txt", basketballTeam5, soccerTeam11)
-# fileWriterUnion("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/difference_baseball_basketball.txt", baseballTeam9, basketballTeam5)
+    try:
+        difference = set1 - set2
+        with open(file, "w") as f:
+            for item in difference:
+                f.write(item + " ")
+    except Exception as e:
+        print(f"Error writing to {file}: {e}")
 
 nestedTuple = (1,2,(3,4,(5,6)))
 
 def fileWriterNestedTuple(file, nested):
-    with open(file, "w") as file:
-        file.write(str(nested))
-
-# fileWriterNestedTuple("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/nestedtuple.txt", nestedTuple)
+    try:
+        with open(file, "w") as f:
+            f.write(str(nested))
+    except Exception as e:
+        print(f"Error writing to {file}: {e}")
 
 def nestedTuples(nested):
     flatTuple = []
     for item in nested:
-        if (type(item) == tuple):
+        if isinstance(item, tuple):
             flatTuple.extend(nestedTuples(item))
         else:
             flatTuple.append(item)
     return flatTuple
-            
-# print(nestedTuples(nestedTuple))
-# fileWriterNestedTuple("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/flattenedTuple.txt")
 
-amount = int(input("Enter the range: "))
-evenNumbers = [i for i in range(amount) if i % 2 == 0]
-squaredNumbers = [i**2 for i in range(amount)]
-print(evenNumbers)
-print(squaredNumbers)
+try:
+    amount = int(input("Enter the range: "))
+    evenNumbers = [i for i in range(amount) if i % 2 == 0]
+    squaredNumbers = [i**2 for i in range(amount)]
+    
+    print(evenNumbers)
+    print(squaredNumbers)
+
+    fileWriter("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/squaredNumbers.txt", squaredNumbers)
+    fileWriter("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/evenNumbers.txt", evenNumbers)
+except ValueError:
+    print("Please enter a valid integer.")
+except Exception as e:
+    print(f"Unexpected error: {e}")
+
+fileWriter("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/basketball_set.txt", basketballTeam5)
+fileWriter("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/baseball_set.txt", baseballTeam9)
+fileWriter("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/soccer_set.txt", soccerTeam11)
+
+fileWriterIntersections("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/intersection_basketball_soccer.txt", basketballTeam5, soccerTeam11)
+fileWriterIntersections("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/intersection_baseball_soccer.txt", baseballTeam9, soccerTeam11)
+
+fileWriterUnion("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/union_basketball_baseball.txt", basketballTeam5, baseballTeam9)
+
+fileWriterDifference("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/difference_basketball_soccer.txt", basketballTeam5, soccerTeam11)
+fileWriterDifference("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/difference_baseball_basketball.txt", baseballTeam9, basketballTeam5)
+
+fileWriterNestedTuple("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/nestedtuple.txt", nestedTuple)
+
+flattenedTuple = nestedTuples(nestedTuple)
+fileWriter("C:/Programming/Semester 3/CPAN214 - Python/Lab6/data/flattenedTuple.txt", flattenedTuple)
